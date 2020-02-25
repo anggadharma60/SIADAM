fcaf805d67cd733a67d82b891363e42f7639653c
anggadharma60 <56443983+anggadharma60@users.noreply.github.com>
2020-02-25 15:23:04 +0700

    Update chart.php

--- application/views/dashboard/chart.php
+++ application/views/dashboard/chart.php
@@ -9,7 +9,7 @@
     </ol>
 </section>
 <?php $data = json_decode($chart);
-// print_r($data->total);
+    // print_r($data->total);
 ?>
 
 <!-- Main content -->
@@ -19,57 +19,120 @@
         <div class="col-md-12">
             <div class="box box-danger">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Statistik Data</h3>
+                    <!-- <h3 class="box-title"></h3> -->
+                    <div class="box-tools pull-right">
+                        <!-- <button type="button" class="btn btn-box-tool" data-widget="collapse">
+                            <i class="fa fa-minus"></i>
+                        </button> -->
+                    </div>
+                    <div class="box-body text-center">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h3>Total Rekap Validasi</h3>
+                                <?php 
+                                if(isset($totalValidasi)){
+                                    echo '<h4>'.$totalValidasi.'</h4>';
+                                }else{
+                                    echo '<h4>'.'Tidak Ada Data'.'</h4>';
+                                }
+                                ?>
+                            </div>
+                            <div class="col-md-4">
+                                <h3>Total Rekap ODP</h3>
+                                <?php 
+                                if(!isset($totalODP) or !isset($totalValidasi)){
+                                    echo '<h4>'.'Tidak Ada Data'.'</h4>';
+                                }else{
+                                    echo '<h4>'.($totalODP-$totalValidasi).'</h4>';
+                                    
+                                }
+                                ?>
+                            </div>
+                            <div class="col-md-4">
+                                <h3>Total Data</h3>
+                                <?php 
+                                if(isset($totalODP)){
+                                    echo '<h4>'.$totalODP.'</h4>';
+                                }else{
+                                    echo '<h4>'.'Tidak Ada Data'.'</h4>';
+                                }
+                                ?>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            <!-- /.box-body -->
+            </div>
+             <!-- /.box -->
+        </div>
+    </div>
+    <?php 
+        for($i=0;$i<$totalSTO;$i++) { ?>
+            <?php 
+            if($i%2==1) {?>
+                <div class="row">
+
+            <?php }?>
+            <div class="col-md-6">
+            <div class="box box-danger">
+                <div class="box-header with-border">
+                    <h3 class="box-title"><?=$namaSTO[$i]?></h3>
                     <div class="box-tools pull-right">
                         <button type="button" class="btn btn-box-tool" data-widget="collapse">
                             <i class="fa fa-minus"></i>
                         </button>
                     </div>
-
                 </div>
-
-                <!-- /.box-body -->
+                <div class="box-body text-center">
+                
+                    <canvas id="<?=$i?>" style="height:250px"></canvas>
+                    
+                    <br>
+                    <label>
+                    <?php
+                
+                        if($data->total!=null){
+                            echo "Rekap Validasi :".$data->total[$i]."&emsp;&emsp;";
+                        }else{
+                            echo "Rekap Validasi :"."&emsp;&emsp;";
+                        }
+                        if($data->totalODP !=null and $data->totalValidasi!=null){
+                            echo "Rekap ODP :".($data->grand_total[$i]-$data->total[$i])."&emsp;&emsp;";
+                        }else{
+                            echo "Rekap ODP :"."&emsp;&emsp;";
+                        }
+                        if($data->grand_total!=null){
+                            echo "Total : ".$data->grand_total[$i]."&emsp;&emsp;";
+                        }else{
+                            echo "Total :"."&emsp;&emsp;";
+                        }
+                        
+                    ?>
+            
+                    </label>
+                </div>
+            <!-- /.box-body -->
             </div>
-            <!-- /.box -->
+             <!-- /.box -->
         </div>
-    </div>
-    <?php
-    for ($i = 0; $i < $totalSTO; $i++) { ?>
-        <?php
-        if ($i % 2 == 1) { ?>
-            <div class="row">
-
-            <?php } ?>
-            <div class="col-md-6">
-                <div class="box box-danger">
-                    <div class="box-header with-border">
-                        <h3 class="box-title"><?= $data->namaSTO[$i] ?></h3>
-                        <div class="box-tools pull-right">
-                            <button type="button" class="btn btn-box-tool" data-widget="collapse">
-                                <i class="fa fa-minus"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="box-body">
-                        <canvas id="<?= $i ?>" style="height:250px"></canvas>
-                    </div>
-                    <!-- /.box-body -->
+            <?php 
+            if($i%2==1) {?>
                 </div>
-                <!-- /.box -->
-            </div>
-            <?php
-            if ($i % 2 == 1) { ?>
-            </div>
 
-        <?php } ?>
-    <?php  } ?>
-    </div>
-
+            <?php }?>
+      <?php  } ?>
+   
+    
+    </section>    
     <script>
-        var chart = JSON.parse('<?php echo $chart ?>');
+ 
+        var chart = JSON.parse('<?php echo $chart?>');
         var n;
         var m;
-
+        
+        
         var config = new Array();
         for(n=0;n<chart.totalSTO;n++){
             // console.log(chart.total[n]);
@@ -80,7 +143,8 @@
                     datasets: [{
                         data: [
                             chart.total[n],
-                            chart.grand_total[n],
+                            chart.grand_total[n]-chart.total[n],
+                           
                         ],
                         backgroundColor: [
                             'rgba(81, 244, 40, 0.95)',
@@ -89,14 +153,14 @@
                         label: 'Dataset 1'
                     }],
                     labels: [
-                        'Rekap ODP',
                         'Rekap Validasi',
+                        'Rekap ODP',
                     ]
                 },
                 options: {
                     responsive: true,
                     legend: {
-                        position: 'bottom',
+                        position: 'top',
                     },
                     title: {
                         display: false,
@@ -109,19 +173,24 @@
                 }
             };
 
-
+		
         }
 
         var ctx = new Array();
         window.onload = function() {
-            for (m = 0; m < chart.totalSTO; m++) {
+            for(m=0;m<chart.totalSTO;m++){
                 ctx[m] = document.getElementById(m).getContext('2d');
-                // ctx[1] = document.getElementById(1).getContext('2d');
+            // ctx[1] = document.getElementById(1).getContext('2d');
                 window.myDoughnut = new Chart(ctx[m], config[m]);
-                // window.myDoughnut = new Chart(ctx[1], config[2]);
+            // window.myDoughnut = new Chart(ctx[1], config[2]);
             }
 
+            
+		};
+		
 
-        };
-    </script>
-</section>
+
+
+	</script>
+
+
